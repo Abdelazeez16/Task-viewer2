c5f0025be0eb80adb884b72e491856d7a166249f
Zezo Farouk <zezofarouk11815371537@gmail.com>
2023-08-16 23:00:05 +0300

    Commit message

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button @click="$emit('toggle-add-task')" :style="{background:color}" class="btn">{{title}}</button>
+</template>
+
+<script>
+export default {
+    name:'MButton',
+    props: {
+        title:String,
+        color:String,
+    },
+}
+</script>
